7aa91b984a81d63915e482e3c6afa672e5c2f5e5
Matt <mmacari9@gmail.com pwd>
2024-06-14 20:39:22 -0700

    fixed js to allow numbers of more than one digit to be used

--- js/calculator.js
+++ js/calculator.js
@@ -38,7 +38,7 @@ function divide(a, b) {
 let num1 = "";
 let num2 = "";
 let operator;
-let clicked = "";
+let clicked = false;
 
 
 function operate(opeartor, number1, number2) {
@@ -65,119 +65,120 @@ const display = document.querySelector('.display');
 const oneBtn = document.querySelector('.one');
 
 oneBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 1;
+
+    if (clicked != true) {
+        num1 += 1;
         display.textContent = num1;
     }
     else {
-        num2 = 1;
+        num2 += 1;
         display.textContent = num2;
     }
 });
 
 const twoBtn = document.querySelector('.two');
 twoBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 2;
+    if (clicked != true) {
+        num1 += 2;
         display.textContent = num1;
     }
     else {
-        num2 = 2;
+        num2 += 2;
         display.textContent = num2;
     }
 });
 const threeBtn = document.querySelector('.three');
 threeBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 3;
+    if (clicked != true) {
+        num1 += 3;
         display.textContent = num1;
     }
     else {
-        num2 = 3;
+        num2 += 3;
         display.textContent = num2;
     }
 });
 
 const fourBtn = document.querySelector('.four');
 fourBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 3;
+    if (clicked != true) {
+        num1 += 4;
         display.textContent = num1;
     }
     else {
-        num2 = 3;
+        num2 += 4;
         display.textContent = num2;
     }
 });
 
 const fiveBtn = document.querySelector('.five');
 fiveBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 5;
+    if (clicked != true) {
+        num1 += 5;
         display.textContent = num1;
     }
     else {
-        num2 = 5;
+        num2 += 5;
         display.textContent = num2;
     }
 });
 
 const sixBtn = document.querySelector('.six');
 sixBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 6;
+    if (clicked != true) {
+        num1 += 6;
         display.textContent = num1;
     }
     else {
-        num2 = 6;
+        num2 += 6;
         display.textContent = num2;
     }
 });
 
 const sevenBtn = document.querySelector('.seven');
 sevenBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 7;
+    if (clicked != true) {
+        num1 += 7;
         display.textContent = num1;
     }
     else {
-        num2 = 7;
+        num2 += 7;
         display.textContent = num2;
     }
 });
 
 const eightBtn = document.querySelector('.eight');
 eightBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 8;
+    if (clicked != true) {
+        num1 += 8;
         display.textContent = num1;
     }
     else {
-        num2 = 8;
+        num2 += 8;
         display.textContent = num2;
     }
 });
 
 const nineBtn = document.querySelector('.nine');
 nineBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 9;
+    if (clicked != true) {
+        num1 += 9;
         display.textContent = num1;
     }
     else {
-        num2 = 9;
+        num2 += 9;
         display.textContent = num2;
     }
 });
 
 const zeroBtn = document.querySelector('.zero');
 zeroBtn.addEventListener("click", function () {
-    if (clicked != "true") {
-        num1 = 0;
+    if (clicked != true) {
+        num1 += 0;
         display.textContent = num1;
     }
     else {
-        num2 = 0;
+        num2 += 0;
         display.textContent = num2;
     }
 });
@@ -188,30 +189,30 @@ clearBtn.addEventListener("click", function () {
     num1 = "";
     num2 = "";
     operator = "";
-    clicked = "";
+    clicked = false;
 });
 
 const divideBtn = document.querySelector('.divide');
 divideBtn.addEventListener("click", function () {
-    clicked = "true";
+    clicked = true;
     operator = divide;
 });
 
 const multBtn = document.querySelector('.multiply');
 multBtn.addEventListener("click", function () {
-    clicked = "true";
+    clicked = true;
     operator = mult;
 });
 
 const subBtn = document.querySelector('.sub');
 subBtn.addEventListener("click", function () {
-    clicked = "true";
+    clicked = true;
     operator = sub;
 });
 
 const addBtn = document.querySelector('.add');
 addBtn.addEventListener("click", function () {
-    clicked = "true";
+    clicked = true;
     operator = add;
     console.log(clicked);
     console.log(num1);
@@ -227,15 +228,15 @@ equalBtn.addEventListener("click", function () {
     if (num1 === "" || num2 === "") {
         display.textContent = "error";
     }
-    else if (operator(num1, num2) % 1 != 0) {
+    else if (operator(parseFloat(num1), parseFloat(num2)) % 1 != 0) {
         display.textContent = operator(num1, num2).toFixed(10);
     }
     else {
-        display.textContent = operator(num1, num2);
+        display.textContent = operator(parseFloat(num1), parseFloat(num2));
     }
     num1 = "";
     num2 = "";
     operator = "";
-    clicked = "";
+    clicked = false;
 
 });
